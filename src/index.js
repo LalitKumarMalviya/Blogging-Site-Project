2fad0767c557abb84aa45c5eef9f68f93a12bca5
lalitqodic <129066211+lalitqodic@users.noreply.github.com>
2023-10-07 13:07:49 +0530

    changes

--- src/index.js
+++ src/index.js
@@ -7,6 +7,10 @@ const app = express()
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use((req,res,next) => {
+    console.log({method: req.method, url: req.url})
+})
+
 mongoose.connect('mongodb+srv://Lalit:g1b1eD2zYIwUl67Z@cluster0.xmtgwuj.mongodb.net/Lalit-Db', {
     useNewUrlParser: true
 })
